a199955ec994164081678bdb6d1155962e661cd1
TheYanusz <javaupdate8.151@gmail.com>
2022-01-07 12:37:19 +0100

    rice

---
+++ .dwm/autostart.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+xrandr --addmode DisplayPort-1 1920x1080
+xrandr --output DisplayPort-1 --mode 1920x1080
+xrandr --output DisplayPort-0 --mode 1920x1200
+xrandr --output DisplayPort-0 --left-of DisplayPort-1
+feh --bg-scale ~/.wallpaper.jpg
+picom --experimental-backend &
+setxkbmap pl
